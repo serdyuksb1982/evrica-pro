c18ff567aa2245e822343f831fea1d223acfe734
79267 <serdyuksb@gmail.com>
2023-08-17 17:19:24 +0300

    eureka config server added

--- eureka-client/src/main/java/ru/serdiuk/controller/TestController.java
+++ eureka-client/src/main/java/ru/serdiuk/controller/TestController.java
@@ -1,5 +1,6 @@
 package ru.serdiuk.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +15,14 @@ public class TestController {
     @Value("${eureka.instance.instance-id}")
     private String id;
 
+    @Value("${tmpVar}")
+    private int tmpVar;
+
+
     @GetMapping("/date")
     public String test() {
         //TODO .......
         LocalDate localDate = LocalDate.now();
-        return localDate + " id = " + id;
+        return localDate + " id = " + id + " : " + tmpVar;
     }
 }
